9b04e2a9b29c3d50ac4966f44ee58a1b0b659310
school laptop <muhammad.shamaeem@elev.ga.ntig.se>
2023-03-10 01:55:45 +0100

    replace mongodb with postgres

---
+++ backend/src/main/java/com/Notes/NoteRepository/NoteRepository.java
@@ -0,0 +1,8 @@
+package com.Notes.NoteRepository;
+
+import com.Notes.Model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
