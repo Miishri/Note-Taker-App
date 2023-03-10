6cf532bc209890457af8343500ff519055492e23
school laptop <muhammad.shamaeem@elev.ga.ntig.se>
2023-03-10 10:33:46 +0100

    add endpoints for controller & improve model

--- backend/src/main/java/com/Notes/Controller/NoteRestController.java
+++ backend/src/main/java/com/Notes/Controller/NoteRestController.java
@@ -1,13 +1,12 @@
 package com.Notes.Controller;
 
-
-import com.Notes.NoteApplication;
+import com.Notes.Model.Note;
+import com.Notes.NoteNotFoundException.NoteNotFoundException;
 import com.Notes.NoteRepository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -21,4 +20,24 @@ public class NoteRestController {
         this.noteRepository = noteRepo;
     }
 
+    @GetMapping("/{id}")
+    public Note getNote(@PathVariable long id) {
+        return noteRepository.findById(id).orElseThrow(() -> new NoteNotFoundException());
+    }
+
+    @PostMapping("/new")
+    public Note createNote(@RequestBody Note newNote) {
+        return noteRepository.save(newNote);
+    }
+
+    @PutMapping("/change/{id}")
+    public Note editNote(@PathVariable long id, @RequestBody Note newNote) {
+        Optional<Note> note = noteRepository.findById(id);
+
+        if (note.isEmpty()) {
+            throw new NoteNotFoundException();
+        }
+        note.get().setNoteData(newNote.getNoteData());
+        return noteRepository.save(note.get());
+    }
 }
--- backend/src/main/java/com/Notes/Model/Note.java
+++ backend/src/main/java/com/Notes/Model/Note.java
@@ -1,24 +1,35 @@
 package com.Notes.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Table(name = "Notes")
 public class Note {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @Column(name = "note_name")
     private String name;
 
-    private String user;
-
-    private String noteName;
-
+    @Column(name = "creation_date")
     private String createDate;
 
+    @Column(name = "note_data")
+    private String noteData;
+
     public Note(){}
-    public Note(long id, String name, String user, String noteName, String createDate) {
+    public Note(long id, String name, String noteData, String createDate) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
         this.id = id;
         this.name = name;
-        this.user = user;
-        this.noteName = noteName;
-        this.createDate = createDate;
+        this.createDate = dtf.format(now);
     }
 
     public long getId() {
@@ -37,27 +48,15 @@ public class Note {
         this.name = name;
     }
 
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getNoteName() {
-        return noteName;
-    }
-
-    public void setNoteName(String noteName) {
-        this.noteName = noteName;
-    }
-
     public String getCreateDate() {
         return createDate;
     }
 
-    public void setCreateDate(String createDate) {
-        this.createDate = createDate;
+    public String getNoteData() {
+        return noteData;
     }
+    public void setNoteData(String noteData) {
+        this.noteData = noteData;
+    }
+
 }
